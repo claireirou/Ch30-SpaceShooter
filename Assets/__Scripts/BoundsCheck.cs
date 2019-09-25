f83a07224f3f5e775b9fe7f7aea8d66d5a7f75ef
Claire Irou <c.iroudayassamy@gmail.com>
2019-09-25 13:58:40 -0400

    Enemies and their scripts
    Created the 5 enemy prefabs and the script for them. Modified the BoundsCheck script to allow it to be used for enemies.

--- Assets/__Scripts/BoundsCheck.cs
+++ Assets/__Scripts/BoundsCheck.cs
@@ -11,10 +11,15 @@ public class BoundsCheck : MonoBehaviour
 {
     [Header("Set in Inspector")]
     public float radius = 1f;
+    public bool keepOnScreen = true;
 
     [Header("Set Dynamically")]
+    public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
+
+    [HideInInspector]
+    public bool offRight, offLeft, offUp, offDown;
 
     private void Awake()
     {
@@ -22,36 +27,40 @@ public class BoundsCheck : MonoBehaviour
         camWidth = camHeight * Camera.main.aspect;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void LateUpdate()
     {
         Vector3 pos = transform.position;
+        isOnScreen = true;
+        offRight = offLeft = offUp = offDown = false;
+
         if(pos.x > camWidth - radius)
         {
             pos.x = camWidth - radius;
+            offRight = true;
         }
-
         if(pos.x < -camWidth + radius)
         {
             pos.x = -camWidth + radius;
+            offLeft = true;
         }
-
         if(pos.y > camHeight - radius)
         {
             pos.y = camHeight - radius;
+            offUp = true;
         }
-
         if(pos.y < -camHeight + radius)
         {
             pos.y = -camHeight + radius;
+            offDown = true;
         }
 
-        transform.position = pos;
+        isOnScreen = !(offRight || offLeft || offUp || offDown);
+        if(keepOnScreen && !isOnScreen)
+        {
+            transform.position = pos;
+            isOnScreen = true;
+            offRight = offLeft = offUp = offDown = false;
+        }
     }
 
     private void OnDrawGizmos()
