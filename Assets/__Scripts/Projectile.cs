1c30e964b7b35cc7dc48db32e0dfed9025c9e536
Claire Irou <c.iroudayassamy@gmail.com>
2019-10-02 14:37:15 -0400

    Oops... Re: Modifying Projectile Class
    This has the modified projectile class to allow use of WeaponDefinitions

--- Assets/__Scripts/Projectile.cs
+++ Assets/__Scripts/Projectile.cs
@@ -6,10 +6,31 @@ public class Projectile : MonoBehaviour
 {
 
     private BoundsCheck bndCheck;
+    private Renderer rend;
+
+    [Header("Set Dynamically")]
+    public Rigidbody rigid;
+    [SerializeField]
+    private WeaponType _type;
+
+    // This public property masks the field _type and takes action when it is set
+    public WeaponType type
+    {
+        get
+        {
+            return (_type);
+        }
+        set
+        {
+            SetType(value);
+        }
+    }
 
     private void Awake()
     {
         bndCheck = GetComponent<BoundsCheck>();
+        rend = GetComponent<Renderer>();
+        rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,5 +40,18 @@ public class Projectile : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    /// <summary>
+    /// Sets the _type private field and colors this projectile to match the 
+    /// WeaponDefinition.
+    /// </summary>
+    /// <param name="eType"> The WeaponType to use. </param>"
+    public void SetType(WeaponType eType)
+    {
+        // Set the type
+        _type = eType;
+        WeaponDefinition def = Main.GetWeaponDefinition(_type);
+        rend.material.color = def.projectileColor;
     }
 }
